1d7e4a8257d4acc9dac24ea6d525622a5f7363a0
stujo <stujo@imagescript.com>
2014-05-09 16:49:33 -0700

    Version sgv31,master,72c7b677c22b6f594743800c0dd4e44dbe07adab,2014-05-09 16:49:33 -0700

--- config/initializers/stujo_git_info.rb
+++ config/initializers/stujo_git_info.rb
@@ -7,13 +7,13 @@ module Stujo
       'master'
     end
     def self.raw_current_commit
-      '52d6b4a9f58bee97aee31fd8193540c90e962baf'
+      '72c7b677c22b6f594743800c0dd4e44dbe07adab'
     end
     def self.raw_current_update
-      '2014-05-09 16:46:37 -0700'
+      '2014-05-09 16:49:33 -0700'
     end
     def self.raw_current_tag
-      'sgv30'
+      'sgv31'
     end
   end
 end
