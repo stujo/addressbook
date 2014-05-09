99a3db2124910b2ae64aa69422d499e1b2d6b0b2
stujo <stujo@imagescript.com>
2014-05-09 14:46:09 -0700

    Trying to get info file onto heroku

---
+++ config/initializers/stujo_git_info.rb
@@ -0,0 +1,9 @@
+
+module Stujo
+  module Git
+    CURRENT_BRANCH = 'release'
+    CURRENT_COMMIT = '7738d027063ae3234b8b4d7ad51d127c36d84d24'
+    UPDATED_ON = '2014-05-09 14:36:19 -0700'
+  end
+end
+
