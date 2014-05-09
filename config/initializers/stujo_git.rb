3b2e1337ceaf59b84ce7ecdfc2befed897a27cc5
stujo <stujo@imagescript.com>
2014-05-09 16:53:26 -0700

    Removing puts from debugging

--- config/initializers/stujo_git.rb
+++ config/initializers/stujo_git.rb
@@ -2,9 +2,6 @@ module Stujo
   module Git
     def self.method_missing(m, *args, &block)
       raw = "raw_#{m}".to_sym
-
-      puts "Checking for #{raw}"
-
       if self.respond_to? raw
         self.send raw # no args or block
       else
