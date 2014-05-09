23b0df16299a4fe1f6e355c488826deac54412e4
stujo <stujo@imagescript.com>
2014-05-09 15:27:16 -0700

    Version sgv9,master,d964d690cfdb93b4c56baaaf66fe1ed0810c6290,2014-05-09 15:27:16 -0700

--- config/initializers/stujo_git_info.rb
+++ config/initializers/stujo_git_info.rb
@@ -1,8 +1,8 @@
 module Stujo
   module Git
-    CURRENT_BRANCH = 'release'
-    CURRENT_COMMIT = '830974915d7427c310ff2a357d41427f92c74bd6'
-    UPDATED_ON = '2014-05-09 15:19:45 -0700'
-    SGV_TAG = 'sgv8'
+    CURRENT_BRANCH = 'master'
+    CURRENT_COMMIT = 'd964d690cfdb93b4c56baaaf66fe1ed0810c6290'
+    UPDATED_ON = '2014-05-09 15:27:16 -0700'
+    SGV_TAG = 'sgv9'
   end
 end
