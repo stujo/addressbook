8b69dddf43e59aa9d64997884d845b02066eec07
stujo <stujo@imagescript.com>
2014-05-09 16:12:27 -0700

    Version sgv13,master,af6eb0a0fb740f3bfeef306fc3401e8947f35b2a,2014-05-09 16:12:27 -0700

--- config/initializers/stujo_git_info.rb
+++ config/initializers/stujo_git_info.rb
@@ -1,8 +1,8 @@
 module Stujo
   module Git
     CURRENT_BRANCH = 'master'
-    CURRENT_COMMIT = '865c1d92437b6016438b7eb64e29df56156c3a13'
-    UPDATED_ON = '2014-05-09 16:09:40 -0700'
-    SGV_TAG = 'sgv12'
+    CURRENT_COMMIT = 'af6eb0a0fb740f3bfeef306fc3401e8947f35b2a'
+    UPDATED_ON = '2014-05-09 16:12:27 -0700'
+    SGV_TAG = 'sgv13'
   end
 end
