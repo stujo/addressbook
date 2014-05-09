32214ee66121778caeae37a7dc0649e5017a8de7
stujo <stujo@imagescript.com>
2014-05-09 15:10:32 -0700

    Git Version release,f1d6201aa866c3c46fd5dbeefbda3863d9511dda,2014-05-09 15:10:32 -0700

--- config/initializers/stujo_git_info.rb
+++ config/initializers/stujo_git_info.rb
@@ -1,7 +1,7 @@
 module Stujo
   module Git
     CURRENT_BRANCH = 'release'
-    CURRENT_COMMIT = '6ead332cb1aa2066ffe98701ac8b342291128834'
-    UPDATED_ON = '2014-05-09 15:10:27 -0700'
+    CURRENT_COMMIT = 'f1d6201aa866c3c46fd5dbeefbda3863d9511dda'
+    UPDATED_ON = '2014-05-09 15:10:32 -0700'
   end
 end
