c219fbfcb9667f30a68d009bb2509e1a3ae1674e
stujo <stujo@imagescript.com>
2014-05-09 16:34:47 -0700

    Version sgv25,master,8f76e4842f1b804430c767faf249ae5ea84441f5,2014-05-09 16:34:47 -0700

--- config/initializers/stujo_git_info.rb
+++ config/initializers/stujo_git_info.rb
@@ -2,8 +2,8 @@ module Stujo
   module Git
     CURRENT_REMOTE = 'local'
     CURRENT_BRANCH = 'master'
-    CURRENT_COMMIT = '0a50b8eaacfea6d548ef769f869a6c3268d088e5'
-    UPDATED_ON = '2014-05-09 16:34:42 -0700'
-    SGV_TAG = 'sgv24'
+    CURRENT_COMMIT = '8f76e4842f1b804430c767faf249ae5ea84441f5'
+    UPDATED_ON = '2014-05-09 16:34:47 -0700'
+    SGV_TAG = 'sgv25'
   end
 end
