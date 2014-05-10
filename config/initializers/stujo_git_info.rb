d67f564746c89fdb82b4ed3a362774ad436ca099
stujo <stujo@imagescript.com>
2014-05-09 23:30:03 -0700

    Moving to pre-commit hook

--- config/initializers/stujo_git_info.rb
+++ config/initializers/stujo_git_info.rb
@@ -1,19 +1,19 @@
 module Stujo
   module Git
     def self.raw_current_remote
-      'origin'
+      ''
     end
     def self.raw_current_branch
       'master'
     end
     def self.raw_current_commit
-      '99881e5819625b3e2b3daaffc672ddf367387a0a'
+      '9fee1ea89eb8fd5bc5215052e2df3ec61d5ed970'
     end
     def self.raw_current_update
-      '2014-05-09 23:26:31 -0700'
+      '2014-05-09 23:30:04 -0700'
     end
     def self.raw_current_tag
-      'sgv1399703191'
+      'sgv1399703404'
     end
   end
 end
