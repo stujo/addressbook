037e52b0ca19817e49088e3d9f7f6519eb5aece1
stujo <stujo@imagescript.com>
2014-05-09 17:05:19 -0700

    Version sgv36,master,494abec80f34dce8d9bdbb2d94a2fd10b89e47a8,2014-05-09 17:05:19 -0700

--- config/initializers/stujo_git_info.rb
+++ config/initializers/stujo_git_info.rb
@@ -1,19 +1,19 @@
 module Stujo
   module Git
     def self.raw_current_remote
-      'heroku'
+      'origin'
     end
     def self.raw_current_branch
-      'testv'
+      'master'
     end
     def self.raw_current_commit
-      'c123d7f05cdc4bf7aaacd69a26121870954186c4'
+      '494abec80f34dce8d9bdbb2d94a2fd10b89e47a8'
     end
     def self.raw_current_update
-      '2014-05-09 17:03:17 -0700'
+      '2014-05-09 17:05:19 -0700'
     end
     def self.raw_current_tag
-      'sgv35'
+      'sgv36'
     end
   end
 end
