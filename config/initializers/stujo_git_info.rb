3adfa722ac236436dac6170a0d62a18d4ecad203
stujo <stujo@imagescript.com>
2014-05-09 23:33:33 -0700

    Removing reference to remote in layout

--- config/initializers/stujo_git_info.rb
+++ config/initializers/stujo_git_info.rb
@@ -4,13 +4,13 @@ module Stujo
       'master'
     end
     def self.raw_current_commit
-      'd67f564746c89fdb82b4ed3a362774ad436ca099'
+      '993c2e4ebb7ce70b07f5aaa03de2c99c8c5005f0'
     end
     def self.raw_current_update
-      '2014-05-09 23:32:16 -0700'
+      '2014-05-09 23:33:35 -0700'
     end
     def self.raw_current_tag
-      'sgv1399703536'
+      'sgv1399703615'
     end
   end
 end
