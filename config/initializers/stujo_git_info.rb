860d6dc25b582df384cd94af8bae37ecda9428c1
stujo <stujo@imagescript.com>
2014-05-10 09:15:46 -0700

    Adding top pagination

--- config/initializers/stujo_git_info.rb
+++ config/initializers/stujo_git_info.rb
@@ -4,13 +4,13 @@ module Stujo
       'master'
     end
     def self.raw_current_commit
-      '993c2e4ebb7ce70b07f5aaa03de2c99c8c5005f0'
+      '3adfa722ac236436dac6170a0d62a18d4ecad203'
     end
     def self.raw_current_update
-      '2014-05-09 23:33:35 -0700'
+      '2014-05-10 09:15:49 -0700'
     end
     def self.raw_current_tag
-      'sgv1399703615'
+      'sgv1399738549'
     end
   end
 end
