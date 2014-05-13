d1c9a68827702eb8b24b683bc04c78fe63110f70
stujo <stujo@imagescript.com>
2014-05-13 14:50:15 -0700

    Removing line

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -38,4 +38,6 @@ module ApplicationHelper
     end
 
   end
+
+
 end
--- config/initializers/stujo_git_info.rb
+++ config/initializers/stujo_git_info.rb
@@ -4,7 +4,7 @@ module Stujo
       'add_flash'
     end
     def self.raw_current_timestamp
-      1400017796
+      1400017816
     end
   end
 end
